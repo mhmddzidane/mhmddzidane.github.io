72ed9e5ababdf8fe7e5629f7575cf4a56d908cb3
mhmddzidane <muhammadzidane633@gmail.com>
2022-12-06 11:30:31 +0700

    update cv

--- components/organism/Header.js
+++ components/organism/Header.js
@@ -40,7 +40,7 @@ class Header extends React.Component {
         <p className="absolute top-[25vh] text-white font-light text-5xl left-0 right-0 text-center z-20">
           MUHAMMAD ZIDANE
         </p>
-        <a href="https://drive.google.com/file/d/1YgZHfrlMg2itkdLaEkLQ2FOgoVhA-0Og/view">
+        <a href="https://drive.google.com/file/d/1wTV_JSorTf11GfskHeHcGjy96J9tQKrG/view?usp=share_link">
           <button className="absolute z-20 top-[50vh] text-white text-xl w-1/2 md:w-1/4 left-[25vw] md:left-[37vw] rounded-full text-center border-4 border-white">
             VIEW RESUME
           </button>
